e520902789de04ab5501392891743ab348d03383
LAPTOP-63R77IPG\edtan <40841142@gm.nfu.edu.tw>
2024-05-11 00:54:52 +0800

    edit App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,16 @@
-import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
-import Category from "./components/Category/Category";
-import AOS from "aos";
 import "aos/dist/aos.css";
-import TopProducts from "./components/TopProducts/TopProducts";
-import Introduction from "./components/Introduction/Introduction";
-import Footer from "./components/Footer/Footer";
-import Popup from "./components/Popup/Popup";
-import AboutZhongDao from "./components/AboutZhongDao/AboutZhongDao";
-import ChairWord from "./components/ChairWord/ChairWord";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from "./components/HomePage/HomePage";
+import Events from "./components/Events/Events";
 
 const App = () => {
-  const [orderPopup, setOrderPopup] = React.useState(false);
-
-  const handleOrderPopup = () => {
-    setOrderPopup(!orderPopup);
-  };
-  React.useEffect(() => {
-    AOS.init({
-      offset: 100,
-      duration: 800,
-      easing: "ease-in-sine",
-      delay: 100,
-    });
-    AOS.refresh();
-  }, []);
-
   return (
-    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
-      <Navbar handleOrderPopup={handleOrderPopup} />
-      <Hero handleOrderPopup={handleOrderPopup} />
-      <TopProducts handleOrderPopup={handleOrderPopup} />
-      <Introduction />
-      <AboutZhongDao />
-      <ChairWord />
-      <Category />
-      <Footer />
-      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
-    </div>
+    <Router>
+      <Routes>
+        <Route element={<HomePage />} path={'/'}></Route>
+        <Route element={<Events />} path='/events'></Route>
+      </Routes>
+    </Router>
   );
 };
 
